41d15ebb9ed011b8f37a9b8bbfbe0060a26d264f
Sacha Cardone <sacha.cardone@ynov.com>
2024-12-31 17:51:58 +0100

    Add EVisualState enum for different visual states

---
+++ Runtime/Scripts/Enums/EVisualSate.cs
@@ -0,0 +1,45 @@
+﻿namespace SakyoGame.Lib.Enums {
+
+    /**
+     * <summary>
+     *  Enum for ui visual states.
+     * </summary>
+     */
+    public enum EVisualSate {
+
+        /**
+         * <summary>
+         *  The normal visual state.
+         * </summary>
+         */
+        Normal,
+
+        /**
+         * <summary>
+         *  The highlighted visual state.
+         * </summary>
+         */
+        Highlighted,
+
+        /**
+         * <summary>
+         *  The pressed visual state.
+         * </summary>
+         */
+        Pressed,
+
+        /**
+         * <summary>
+         *  The selected visual state.
+         * </summary>
+         */
+        Selected,
+
+        /**
+         * <summary>
+         *  The disabled visual state.
+         * </summary>
+         */
+        Disabled
+    }
+}
