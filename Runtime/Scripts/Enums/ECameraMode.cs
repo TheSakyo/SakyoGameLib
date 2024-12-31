a17707e5ea854b7eab8b048b70754d8366030d26
Sacha Cardone <sacha.cardone@ynov.com>
2024-12-31 17:51:21 +0100

    Add ECameraMode enum for different camera perspectives

---
+++ Runtime/Scripts/Enums/ECameraMode.cs
@@ -0,0 +1,45 @@
+﻿namespace SakyoGame.Lib.Enums {
+
+    /**
+     * <summary>
+     *  Enum for camera modes.
+     * </summary>
+     */
+    public enum ECameraMode {
+
+        /**
+         * <summary>
+         *  The default camera mode.
+         * </summary>
+         */
+        Default,
+
+        /**
+         * <summary>
+         *  The first person view camera mode.
+         * </summary>
+         */
+        FirstPersonView,
+
+        /**
+         * <summary>
+         *  The front view camera mode.
+         * </summary>
+         */
+        FrontView,
+
+        /**
+         * <summary>
+         *  The third person view camera mode.
+         * </summary>
+         */
+        ThirdPersonView,
+
+        /**
+         * <summary>
+         *  No camera mode.
+         * </summary>
+         */
+        None
+    }
+}
