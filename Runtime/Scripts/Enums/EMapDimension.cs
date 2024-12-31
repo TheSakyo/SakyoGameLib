2c15d93060b31a4bdd53b1f1d13d4739d496773c
Sacha Cardone <sacha.cardone@ynov.com>
2024-12-31 17:52:22 +0100

    Add EMapDimension enum to represent different map dimensions

---
+++ Runtime/Scripts/Enums/EMapDimension.cs
@@ -0,0 +1,24 @@
+﻿namespace SakyoGame.Lib.Enums {
+
+    /**
+     * <summary>
+     *  Enum for map dimensions.
+     * </summary>
+     */
+    public enum EMapDimension {
+
+        /**
+         * <summary>
+         *  The 2D map dimension.
+         * </summary>
+         */
+        TwoDimensional,
+
+        /**
+         * <summary>
+         *  The 3D map dimension.
+         * </summary>
+         */
+        ThreeDimensional
+    }
+}
