996fb4554373065590ba8eb6b39781e91d7afec0
Sacha Cardone <sacha.cardone@ynov.com>
2024-12-31 17:51:42 +0100

    Add EResponsiveMode enum for different responsive design modes

---
+++ Runtime/Scripts/Enums/EResponsiveMode.cs
@@ -0,0 +1,24 @@
+﻿namespace SakyoGame.Lib.Enums {
+
+    /**
+     * <summary>
+     *  Enum for responsive modes.
+     * </summary>
+     */
+    public enum EResponsiveMode {
+
+        /**
+         * <summary>
+         *  The screen size responsive mode.
+         * </summary>
+         */
+        ScreenSize,
+
+        /**
+         * <summary>
+         *  The parent size responsive mode.
+         * </summary>
+         */
+        ParentSize
+    }
+}
