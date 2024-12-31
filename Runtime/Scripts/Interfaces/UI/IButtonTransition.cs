b57044a0f80fb87053c39d8faddcb517889a5b94
Sacha Cardone <sacha.cardone@ynov.com>
2024-12-31 17:55:11 +0100

    Add IButtonTransition interface for defining button transitions with hover state handling

---
+++ Runtime/Scripts/Interfaces/UI/IButtonTransition.cs
@@ -0,0 +1,19 @@
+﻿namespace SakyoGame.Lib.Interfaces.UI {
+
+    /**
+     * <summary>
+     *  Interface for button transitions.
+     * </summary>
+     */
+    public interface IButtonTransition {
+
+        /**
+         * <summary>
+         *  Apply the visual transition of the button depending on whether it is hovered or not.
+         * </summary>
+         * <param name="button"> The button to apply the transition to. </param>
+         * <param name="isHovered"> Whether the button is hovered or not. </param>
+         */
+        public void Apply(ISelectableUI button, bool isHovered);
+    }
+}
