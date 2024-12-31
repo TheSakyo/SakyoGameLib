4a4797f27ef2b590d82780f5c0a9f9127c9b7fe3
Sacha Cardone <sacha.cardone@ynov.com>
2024-12-31 18:05:16 +0100

    Add AnchorAdjustments struct to store X and Y axis adjustment values, including properties and constructors.

---
+++ Runtime/Scripts/Structs/AnchorAdjustments.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SakyoGame.Lib.Structs {
+
+    /**
+     * <summary>
+     *  Struct for storing anchor adjustment values.
+     * </summary>
+     */
+    [Serializable]
+    public struct AnchorAdjustments {
+
+        /************************/
+        /****** PROPERTIES ******/
+        /************************/
+
+        /**
+         * <summary>
+         *  The X-axis adjustment.
+         * </summary>
+         */
+        public float xAxis;
+
+        /**
+         * <summary>
+         *  The Y-axis adjustment.
+         * </summary>
+         */
+        public float yAxis; // Y-axis adjustment
+
+        /**************************/
+        /****** CONSTRUCTORS ******/
+        /**************************/
+
+        /**
+         * <summary>
+         *  Initializes a new instance of the <see cref="AnchorAdjustments"/> struct.
+         * </summary>
+         * <param name="x">The X-axis adjustment.</param>
+         * <param name="y">The Y-axis adjustment.</param>
+         */
+        public AnchorAdjustments(float x, float y) { xAxis = x; yAxis = y; }
+    }
+}
