cf230c79d81cc73ba691e9e266d6196af97ad0ee
Sacha Cardone <sacha.cardone@ynov.com>
2024-12-31 17:53:10 +0100

    Add EComparisonType enum for various comparison operations.

---
+++ Runtime/Scripts/Enums/EComparisonType.cs
@@ -0,0 +1,52 @@
+﻿namespace SakyoGame.Lib.Enums {
+
+    /**
+     * <summary>
+     *  Enum for comparison types.
+     * </summary>
+     */
+    public enum EComparisonType {
+
+        /**
+         * <summary>
+         *  The comparison type for checking if a value is greater than another.
+         * </summary>
+         */
+        Equal,
+
+        /**
+         * <summary>
+         *  The comparison type for checking if a value is not equal to another.
+         * </summary>
+         */
+        NotEqual,
+
+        /**
+         * <summary>
+         *  The comparison type for checking if a value is greater than another.
+         * </summary>
+         */
+        Greater,
+
+        /**
+         * <summary>
+         *  The comparison type for checking if a value is greater than or equal to another.
+         * </summary>
+         */
+        GreaterOrEqual,
+
+        /**
+         * <summary>
+         *  The comparison type for checking if a value is less than another.
+         * </summary>
+         */
+        Less,
+
+        /**
+         * <summary>
+         *  The comparison type for checking if a value is less than or equal to another.
+         * </summary>
+         */
+        LessOrEqual
+    }
+}
