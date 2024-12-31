767cc7730f2e124d45c9835b615a3f425045f604
Sacha Cardone <sacha.cardone@ynov.com>
2024-12-31 17:54:37 +0100

    Add IPrefab interface for defining prefabs with minimum distance, spawn, and unspawn methods

---
+++ Runtime/Scripts/Interfaces/Actors/IPrefab.cs
@@ -0,0 +1,38 @@
+﻿using SakyoGame.Lib.Maps.Meshes;
+using SakyoGame.Lib.Structs;
+using SakyoGame.Lib.Actors;
+
+namespace SakyoGame.Lib.Interfaces.Actors {
+
+    /**
+     * <summary>
+     *  Interface for prefabs (<see cref="BasePrefab"/>).
+     * </summary>
+     */
+    public interface IPrefab {
+
+        /**
+         * <summary>
+         * The minimum distance between two same prefab can be spawned.
+         * </summary>
+         */
+        float MinimumDistance { get; }
+
+        /**
+        * <summary>
+        *  Tries spawns prefabs at random positions on the mesh based on the provided height map and mesh data.
+        *  Prefabs are instantiated only if the number of prefabs is below the specified limit.
+        * </summary>
+        * <param name="meshData">The mesh data containing information about the terrain.</param>
+         * <param name="regions">An array of <see cref="TerrainType"/> structs containing information about terrain types.</param>
+        */
+        public void TrySpawn(MeshData meshData, TerrainType[] regions);
+
+        /**
+          * <summary>
+          *  Tries destroy prefabs.
+          * </summary>
+          */
+        public void TryUnSpawn();
+    }
+}
