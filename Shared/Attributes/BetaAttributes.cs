af8e8d90d2b304b2a0bb61a6c51b99a63179b6bd
Sacha Crd <sacha.cardone@gmail.com>
2025-01-05 12:46:57 +0100

    Add Custom Attribute for beta functionality

---
+++ Shared/Attributes/BetaAttributes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SakyoGame.Lib.Shared.Attributes {
+
+    /**
+     * <summary>
+     *  Attribute to mark features that are in beta and may not be fully functional or stable.
+     * </summary>
+     */
+    [AttributeUsage(AttributeTargets.All)]
+    public class BetaAttribute : Attribute {
+
+        /**
+         * <summary>
+         *  The reason why this feature is in beta.
+         * </summary>
+         */
+        public string Reason { get; }
+
+        /**
+         * <summary>
+         *  Constructor for the BetaAttribute.
+         * </summary>
+         * <param name="reason">The reason why this feature is in beta.</param>
+         */
+        public BetaAttribute(string reason = "This feature is in beta.") { Reason = reason; }
+    }
+}
